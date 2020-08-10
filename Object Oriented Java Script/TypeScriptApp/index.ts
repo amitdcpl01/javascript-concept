c7889b5e02e60236d54ee921536f7caed828724b
AMIT KUMAR <amitkumar028@in.ibm.com>
2020-08-10 09:10:46 +0530

    typescript changes

--- Object Oriented Java Script/TypeScriptApp/index.ts
+++ Object Oriented Java Script/TypeScriptApp/index.ts
@@ -24,4 +24,14 @@ axios.get(url).then((response) => {
   Has a title of : ${title}
   Is it finished: ${completed}
   `);
+
+  logTodo(id, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+  The Todo with ID:${id}
+  Has a title of : ${title}
+  Is it finished: ${completed}
+  `);
+};
