c6b90a1d8e7ecc2745a51ca7fb548603088fc2ca
AMIT KUMAR <amitkumar028@in.ibm.com>
2020-08-18 01:26:42 +0530

    added function, interface

---
+++ Object Oriented Java Script/TypeScriptSecondApp/simple-typescript/dist/interfaces.js
@@ -0,0 +1,10 @@
+"use strict";
+let profile = {
+    name: 'John'
+};
+const a = { someProp: 'some prop' };
+a.x = 1;
+a.y = 2;
+const sum = (a, b) => a + b;
+sum.prop1 = 'some prop';
+let child = { x: 'some prop', y: 'y prop', z: 'z prop' };
