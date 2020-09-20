2705f05b8b2217bce551ebbcf211cfccddee62d5
AMIT KUMAR <amitkumar028@in.ibm.com>
2020-09-20 14:31:03 +0530

    added counter app

--- Object Oriented Java Script/todo-typescript/src/index.tsx
+++ Object Oriented Java Script/todo-typescript/src/index.tsx
@@ -5,27 +5,39 @@ interface AppProps {
   color?: string; // ? because if we donot want to pass color pops in <App>, it make it optional
 }
 
-class App extends React.Component<AppProps> {
-  state = { counter: 0 };
+// // interface AppState {
+// //   counter: number;
+// // }
+// class App extends React.Component<AppProps> {
+//   //class App extends React.Component<AppProps, AppState> {
+//   state = { counter: 0 };
 
-  onIncreament = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
+//   // constructor(props: AppProps) {
+//   //   super(props);
+//   //   this.state = { counter: 0 };
+//   // }
+//   onIncreament = (): void => {
+//     this.setState({ counter: this.state.counter + 1 });
+//   };
 
-  onDecreament = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
+//   onDecreament = (): void => {
+//     this.setState({ counter: this.state.counter - 1 });
+//   };
 
-  render() {
-    return (
-      // <div>Hi there {this.props.color}</div>
-      <div>
-        <button onClick={this.onIncreament}>Increament</button>
-        <button onClick={this.onDecreament}>Decreament</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       // <div>Hi there {this.props.color}</div>
+//       <div>
+//         <button onClick={this.onIncreament}>Increament</button>
+//         <button onClick={this.onDecreament}>Decreament</button>
+//         {this.state.counter}
+//       </div>
+//     );
+//   }
+// }
+
+const App = (props: AppProps): JSX.Element => {
+  return <div>hello</div>;
+};
 
 ReactDOM.render(<App color='red' />, document.querySelector('#root'));
